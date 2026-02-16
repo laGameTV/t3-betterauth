7eb1322f2151d9219cb6a35e5da6b1d71ec6f33a
Lennox <hello@lagametv.de>
2026-02-16 21:12:03 +0100

    Recommended changes for create-t3's Better Auth implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import { LatestPost } from "@/app/_components/post";
-import { auth } from "@/server/better-auth";
-import { getSession } from "@/server/better-auth/server";
+import { auth } from "@/server/auth";
+import { getSession } from "@/server/auth/server";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,10 +1,12 @@
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { env } from "@/env";
 import { db } from "@/server/db";
 
 export const auth = betterAuth({
+  plugins: [nextCookies()], // make sure this is the last plugin in the array
   database: drizzleAdapter(db, {
     provider: "pg", // or "pg" or "mysql"
   }),
@@ -15,7 +17,7 @@ export const auth = betterAuth({
     github: {
       clientId: env.BETTER_AUTH_GITHUB_CLIENT_ID,
       clientSecret: env.BETTER_AUTH_GITHUB_CLIENT_SECRET,
-      redirectURI: "http://localhost:3000/api/auth/callback/github",
+      // redirectURI: "http://localhost:3000/api/auth/callback/github", // DONT HARDOCODE THIS, USE "BETTER_AUTH_URL" ENV VARIABLE INSTEAD
     },
   },
 });
